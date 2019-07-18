47d3449ff98d720d5891b776db9bf782eaf6b6f4
melizza-cely <melizza.cely@gmail.com>
2019-07-18 16:58:43 -0500

    Trabajando con Routes.

--- app/Http/Controllers/PermisoController.php
+++ app/Http/Controllers/PermisoController.php
@@ -11,7 +11,7 @@ class PermisoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($nombre)
+    public function index()
     {
         return view('permiso');
     }
